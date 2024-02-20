081afb736c58cd0409a33b3ad66a6391cf3b8938
allanbc <allan.barrosc@gmail.com>
2024-02-20 04:29:00 -0300

    feat: service com a lógica de negócio

---
+++ src/main/java/br/com/deveficiente/casadocodigov2/service/AutorService.java
@@ -0,0 +1,31 @@
+package br.com.deveficiente.casadocodigov2.service;
+
+import br.com.deveficiente.casadocodigov2.entity.Autor;
+import br.com.deveficiente.casadocodigov2.exception.AutorCadastradoException;
+import br.com.deveficiente.casadocodigov2.model.CadastroAutorRequest;
+import br.com.deveficiente.casadocodigov2.repository.AutorRepository;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Service
+public class AutorService {
+    private static final Logger LOG = getLogger(AutorService.class);
+    private final AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    public Autor create(CadastroAutorRequest autorForm) {
+        LOG.info("Cadastrando autor: {}", autorForm);
+        var verificaCadastro = autorRepository.existsByNomeOrEmail(autorForm.nome(), autorForm.email());
+        if(verificaCadastro) {
+            throw new AutorCadastradoException("Autor já está cadastrado!");
+        }
+        return autorRepository.save(new Autor(autorForm));
+    }
+
+
+}
