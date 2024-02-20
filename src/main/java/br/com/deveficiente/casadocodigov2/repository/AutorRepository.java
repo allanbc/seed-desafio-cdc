b14075b10fbad19254923568bc0deb58f75ecd7f
allanbc <allan.barrosc@gmail.com>
2024-02-20 04:29:27 -0300

    feat: repository Autor

---
+++ src/main/java/br/com/deveficiente/casadocodigov2/repository/AutorRepository.java
@@ -0,0 +1,12 @@
+package br.com.deveficiente.casadocodigov2.repository;
+
+import br.com.deveficiente.casadocodigov2.entity.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    boolean existsByNomeOrEmail(String nome, String email);
+}
