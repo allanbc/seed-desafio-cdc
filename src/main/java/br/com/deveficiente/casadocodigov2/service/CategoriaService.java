78f03e425466e35f83c21453e652ee85e7a55a09
allanbc <allan.barrosc@gmail.com>
2024-02-21 14:30:06 -0300

    feat: add CategoriaService

---
+++ src/main/java/br/com/deveficiente/casadocodigov2/service/CategoriaService.java
@@ -0,0 +1,36 @@
+package br.com.deveficiente.casadocodigov2.service;
+
+import br.com.deveficiente.casadocodigov2.entity.Categoria;
+import br.com.deveficiente.casadocodigov2.exception.CategoriaCadastradaException;
+import br.com.deveficiente.casadocodigov2.model.Categoria.CadastroCategoriaRequest;
+import br.com.deveficiente.casadocodigov2.model.Categoria.CategoriaResponse;
+import br.com.deveficiente.casadocodigov2.repository.CategoriaRepository;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Service
+public class CategoriaService {
+
+    private static final Logger LOG = getLogger(CategoriaService.class);
+
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public Categoria create(CadastroCategoriaRequest request) {
+        LOG.info("Cadastrando categoria: {}", request);
+        var verificaCadastro = categoriaRepository.existsByNomeIgnoreCase(request.nome());
+
+        if(verificaCadastro) {
+            throw new CategoriaCadastradaException("Categoria já está cadastrada!");
+        }
+
+        return categoriaRepository.save(new Categoria(request));
+    }
+}
