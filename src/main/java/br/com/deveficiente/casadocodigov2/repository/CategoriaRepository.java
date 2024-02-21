7a82228ad87ae4d223421a64e3e04f8010d8e1a4
allanbc <allan.barrosc@gmail.com>
2024-02-21 14:32:00 -0300

    feat: add CategoriaRepository

---
+++ src/main/java/br/com/deveficiente/casadocodigov2/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package br.com.deveficiente.casadocodigov2.repository;
+
+import br.com.deveficiente.casadocodigov2.entity.Categoria;
+import br.com.deveficiente.casadocodigov2.model.Categoria.CategoriaResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    boolean existsByNomeIgnoreCase(String nome);
+
+    Optional<Categoria> findByNomeIgnoreCase(String nome);
+}
