5e9291382b85f4e7bd0cd152b24f580a4d7afa32
allanbc <allan.barrosc@gmail.com>
2024-02-26 20:55:15 -0300

    feat: adiciona anotation pra corrigir erro do json

--- src/main/java/br/com/deveficiente/casadocodigov2/entity/Categoria.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/entity/Categoria.java
@@ -1,26 +1,25 @@
 package br.com.deveficiente.casadocodigov2.entity;
 
-import br.com.deveficiente.casadocodigov2.model.Categoria.CadastroCategoriaRequest;
+import br.com.deveficiente.casadocodigov2.model.categoria.CadastroCategoriaRequest;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.ToString;
-
-import java.util.Optional;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
-@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"}, ignoreUnknown = true)
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-
     public Categoria() {
     }
-
     public Categoria(CadastroCategoriaRequest request) {
         this.nome = request.nome();
     }
-
 }
