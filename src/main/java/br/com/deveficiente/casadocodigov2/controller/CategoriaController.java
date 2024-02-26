fd8b818a7e3c5919e46e58f34311c9411f0d9ce6
allanbc <allan.barrosc@gmail.com>
2024-02-26 20:54:12 -0300

    refactor: add log e altera retorno da response

--- src/main/java/br/com/deveficiente/casadocodigov2/controller/CategoriaController.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/controller/CategoriaController.java
@@ -1,11 +1,10 @@
 package br.com.deveficiente.casadocodigov2.controller;
 
-import br.com.deveficiente.casadocodigov2.model.Autor.AutorResponse;
-import br.com.deveficiente.casadocodigov2.model.Autor.CadastroAutorRequest;
-import br.com.deveficiente.casadocodigov2.model.Categoria.CadastroCategoriaRequest;
-import br.com.deveficiente.casadocodigov2.model.Categoria.CategoriaResponse;
+import br.com.deveficiente.casadocodigov2.model.categoria.CadastroCategoriaRequest;
+import br.com.deveficiente.casadocodigov2.model.categoria.CategoriaResponse;
 import br.com.deveficiente.casadocodigov2.service.CategoriaService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
-
+    private static final Logger LOG = getLogger(CategoriaController.class);
     private final CategoriaService categoriaService;
 
     public CategoriaController(CategoriaService categoriaService) {
@@ -25,6 +28,7 @@ public class CategoriaController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CategoriaResponse> create(@RequestBody @Valid CadastroCategoriaRequest request) {
-        return ResponseEntity.ok(CategoriaResponse.toResponse(categoriaService.create(request)));
+        LOG.info("Cadastrando uma categoria");
+        return ResponseEntity.ok(new CategoriaResponse(categoriaService.create(request)));
     }
 }
