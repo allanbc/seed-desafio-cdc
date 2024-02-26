9292fac41ecac889a5816d3c158d7265fd10db27
allanbc <allan.barrosc@gmail.com>
2024-02-26 20:56:02 -0300

    refactor: altera nome do parâmetro da request

--- src/main/java/br/com/deveficiente/casadocodigov2/entity/Autor.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/entity/Autor.java
@@ -1,8 +1,11 @@
 package br.com.deveficiente.casadocodigov2.entity;
 
-import br.com.deveficiente.casadocodigov2.model.Autor.CadastroAutorRequest;
+import br.com.deveficiente.casadocodigov2.model.autor.CadastroAutorRequest;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SourceType;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -10,7 +13,10 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Entity
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"}, ignoreUnknown = true)
 public class Autor {
 
     @Id
@@ -23,12 +29,14 @@ public class Autor {
     private LocalDateTime dateCreated;
     @UpdateTimestamp(source = SourceType.VM)
     private LocalDateTime lastUpdated;
+    @OneToOne(fetch = FetchType.LAZY)
+    private Livro livro;
 
     public Autor() {
     }
-    public Autor(CadastroAutorRequest autorForm) {
-        this.nome = autorForm.nome();
-        this.email = autorForm.email();
-        this.descricao = autorForm.descricao();
+    public Autor(CadastroAutorRequest request) {
+        this.nome = request.nome();
+        this.email = request.email();
+        this.descricao = request.descricao();
     }
 }
