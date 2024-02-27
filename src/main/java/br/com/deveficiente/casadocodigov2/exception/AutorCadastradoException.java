87cf064b0ac26af0b27f9add0684f17c22be6ffd
allanbc <allan.barrosc@gmail.com>
2024-02-27 01:02:06 -0300

    refactor: remove método e espaço em branco

--- src/main/java/br/com/deveficiente/casadocodigov2/exception/AutorCadastradoException.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/exception/AutorCadastradoException.java
@@ -3,7 +3,6 @@ package br.com.deveficiente.casadocodigov2.exception;
 import org.springframework.http.HttpStatus;
 
 public class AutorCadastradoException extends CasaDoCodigoException {
-
     public AutorCadastradoException(String description) {
         super(HttpStatus.CONFLICT, "autor_cad_exception", description);
     }
--- src/main/java/br/com/deveficiente/casadocodigov2/exception/AutorNotFoundException.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/exception/AutorNotFoundException.java
@@ -3,9 +3,6 @@ package br.com.deveficiente.casadocodigov2.exception;
 import org.springframework.http.HttpStatus;
 
 public class AutorNotFoundException extends CasaDoCodigoException {
-    public AutorNotFoundException(String field) {
-        super(HttpStatus.NOT_FOUND, "autor_not_found", "Nenhum autor encontrado com o nome: " + field);
-    }
     public AutorNotFoundException(Long id) {
         super(HttpStatus.NOT_FOUND, "autor_not_found", "Nenhum autor encontrado com o id: " + id);
     }
