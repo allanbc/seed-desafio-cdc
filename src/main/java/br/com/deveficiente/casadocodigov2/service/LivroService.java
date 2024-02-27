cfc0e0dc4b13964d6055234a947fd7306acde9e6
allanbc <allan.barrosc@gmail.com>
2024-02-27 01:00:49 -0300

    feat: service que detalha um livro

--- src/main/java/br/com/deveficiente/casadocodigov2/service/LivroService.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/service/LivroService.java
@@ -3,13 +3,15 @@ package br.com.deveficiente.casadocodigov2.service;
 import br.com.deveficiente.casadocodigov2.entity.Autor;
 import br.com.deveficiente.casadocodigov2.entity.Categoria;
 import br.com.deveficiente.casadocodigov2.entity.Livro;
-import br.com.deveficiente.casadocodigov2.model.autor.AutorResponse;
+import br.com.deveficiente.casadocodigov2.exception.LivroNotFoundException;
 import br.com.deveficiente.casadocodigov2.model.livro.CadastroLivroRequest;
+import br.com.deveficiente.casadocodigov2.model.livro.LivroDetalheResponse;
 import br.com.deveficiente.casadocodigov2.model.livro.LivroResponse;
 import br.com.deveficiente.casadocodigov2.repository.AutorRepository;
 import br.com.deveficiente.casadocodigov2.repository.CategoriaRepository;
 import br.com.deveficiente.casadocodigov2.repository.LivroRepository;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,15 +22,12 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Service
 public class LivroService {
     private static final Logger LOG = getLogger(LivroService.class);
-    private final LivroRepository livroRepository;
-    private final AutorRepository  autorRepository;
-    private final CategoriaRepository categoriaRepository;
-
-    public LivroService(LivroRepository livroRepository, AutorRepository autorRepository, CategoriaRepository categoriaRepository) {
-        this.livroRepository = livroRepository;
-        this.autorRepository = autorRepository;
-        this.categoriaRepository = categoriaRepository;
-    }
+    @Autowired
+    private LivroRepository livroRepository;
+    @Autowired
+    private AutorRepository  autorRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     @Transactional
     public Livro create(CadastroLivroRequest request) {
@@ -44,5 +43,16 @@ public class LivroService {
                 .stream()
                 .map(LivroResponse::new)
                 .toList();
+    }
+
+    public LivroDetalheResponse getByProdutoDetalhe(Long id) {
+        LOG.info("Buscando detalhe de um livro");
+        return livroRepository
+                .findById(id)
+                .map(LivroDetalheResponse::new)
+                .orElseThrow(() -> {
+                    LOG.warn("Erro ao detalhar livro");
+                    return new LivroNotFoundException(id);
+                });
     }
 }
