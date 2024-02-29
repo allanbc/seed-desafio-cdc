c14965867ccbf9dc3153b3425c4b3c98372e7e20
allanbc <allan.barrosc@gmail.com>
2024-02-29 11:06:54 -0300

    refactor: troca classe e remove chamada de País

--- src/main/java/br/com/deveficiente/casadocodigov2/service/EstadoService.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/service/EstadoService.java
@@ -5,7 +5,6 @@ import br.com.deveficiente.casadocodigov2.entity.Pais;
 import br.com.deveficiente.casadocodigov2.repository.EstadoRepository;
 import br.com.deveficiente.casadocodigov2.repository.PaisRepository;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,33 +12,21 @@ import java.util.List;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
-public class DominioService {
-    private static final Logger LOG = getLogger(DominioService.class);
-    @Autowired
-    private EstadoRepository estadoRepository;
+public class EstadoService {
+    private static final Logger LOG = getLogger(EstadoService.class);
+    private final EstadoRepository estadoRepository;
+    private final PaisRepository paisRepository;
 
-    @Autowired
-    private PaisRepository paisRepository;
+    public EstadoService(EstadoRepository estadoRepository, PaisRepository paisRepository) {
+        this.estadoRepository = estadoRepository;
+        this.paisRepository = paisRepository;
+    }
 
     public Estado createEstado(Estado request) {
         LOG.info("Service que cadastra um estado");
         Pais pais = paisRepository.getReferenceById(request.getPais().getId());
         return estadoRepository.save(new Estado().toRequest(request, pais));
     }
-    public Pais createPais(Pais request) {
-        LOG.info("Service que cadastra um pais");
-        return paisRepository.save(new Pais().toRequest(request));
-    }
-
-    public List<Pais> getPaises() {
-        LOG.info("Buscando países");
-        return paisRepository
-                .findAll()
-                .stream()
-                .map(Pais::toResponse)
-                .toList();
-    }
-
     public List<Estado> getEstados() {
         LOG.info("Buscando estados");
         return estadoRepository
---
+++ src/main/java/br/com/deveficiente/casadocodigov2/service/PaisService.java
@@ -0,0 +1,35 @@
+package br.com.deveficiente.casadocodigov2.service;
+
+import br.com.deveficiente.casadocodigov2.entity.Pais;
+import br.com.deveficiente.casadocodigov2.repository.EstadoRepository;
+import br.com.deveficiente.casadocodigov2.repository.PaisRepository;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Service
+public class PaisService {
+    private static final Logger LOG = getLogger(PaisService.class);
+    private final PaisRepository paisRepository;
+
+    public PaisService(PaisRepository paisRepository) {
+        this.paisRepository = paisRepository;
+    }
+
+    public Pais createPais(Pais request) {
+        LOG.info("Service que cadastra um pais");
+        return paisRepository.save(new Pais().toRequest(request));
+    }
+
+    public List<Pais> getPaises() {
+        LOG.info("Buscando países");
+        return paisRepository
+                .findAll()
+                .stream()
+                .map(Pais::toResponse)
+                .toList();
+    }
+}
