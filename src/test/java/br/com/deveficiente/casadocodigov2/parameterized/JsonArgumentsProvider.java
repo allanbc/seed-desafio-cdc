85b1b27daf225952bbc102ed583b5a453ad46a2b
allanbc <allan.barrosc@gmail.com>
2024-06-24 18:11:30 -0300

    refactor: add parameterizedTest

--- src/test/java/br/com/deveficiente/casadocodigov2/parameterized/JsonArgumentsProvider.java
+++ src/test/java/br/com/deveficiente/casadocodigov2/parameterized/JsonArgumentsProvider.java
@@ -1,13 +1,32 @@
 package br.com.deveficiente.casadocodigov2.parameterized;
 
+import br.com.deveficiente.casadocodigov2.entity.Autor;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Label;
+import net.jqwik.api.constraints.AlphaChars;
+import net.jqwik.api.constraints.StringLength;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
+import java.util.Map;
 import java.util.stream.Stream;
 
-public class JsonArgumentsProvider implements ArgumentsProvider {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+public class JsonArgumentsProvider implements ArgumentsProvider {
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
         //ARRANGE
@@ -46,7 +65,6 @@ public class JsonArgumentsProvider implements ArgumentsProvider {
                     "descricao": ""
                 }
                 """;
-
 
         return Stream.of(
                 Arguments.of(json0, 500),
--- src/test/java/br/com/deveficiente/casadocodigov2/service/AutorServiceTest.java
+++ src/test/java/br/com/deveficiente/casadocodigov2/service/AutorServiceTest.java
@@ -5,14 +5,21 @@ import br.com.deveficiente.casadocodigov2.model.autor.CadastroAutorRequest;
 import br.com.deveficiente.casadocodigov2.repository.AutorRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@ActiveProfiles("tests")
 @SpringBootTest
 class AutorServiceTest {
 
@@ -22,48 +29,106 @@ class AutorServiceTest {
     @Autowired
     private AutorRepository autorRepository;
 
-    @Test
-    @DisplayName("Dado os parâmetros para criação, deverá ser possível cadastrar um autor.")
-    void testDeveCriarUmAutor() {
-        System.out.println("Testando...");
+    private static Stream<Arguments> provideAutorRequests() {
+        return Stream.of(
 
-        //Arrange
-        CadastroAutorRequest request = new CadastroAutorRequest("Allan Campos", "teste-dev@gmail.com", "testando");
+                // Caso em que o e-mail já está cadastrado
+                Arguments.of(new CadastroAutorRequest("Allan Eduardo", "teste-dev@gmail.com", "Testando"),
+                        true, // Simular que o autor já existe
+                        "Existe autor cadastrado para o e-mail informado!"),
+                // Caso em que o e-mail é único
+                Arguments.of(new CadastroAutorRequest("Allan Barros", "teste-dev@gmail.com", "Testando"),
+                        false, // Simular que o autor não existe
+                        null) // Nenhuma exceção esperada
 
-        //limpar o repositório antes do teste
-//        autorRepository.deleteAll();
-//
-        // Act
-        Autor result = autorService.create((request));
 
-        //Assert
-        assertNotNull(result);
-        assertEquals(request.nome(), result.getNome());
-        assertEquals(request.email(), result.getEmail());
+        );
     }
 
-    @Test
-    @DisplayName("Deve tentar cadastrar um autor com o mesmo email e deverá retornar uma exceção")
-    void testDeveTentarCriarUmAutorComOMesmoEmailERetornarExcecao() {
+
+//    @Test
+//    @DisplayName("Dado os parâmetros para criação, deverá ser possível cadastrar um autor.")
+//    void testDeveCriarUmAutor() {
+//        System.out.println("Testando...");
+//
+//        //Arrange
+//        CadastroAutorRequest request = new CadastroAutorRequest("Allan Campos", "teste-dev@gmail.com", "testando");
+//
+//        //limpar o repositório antes do teste
+//        autorRepository.deleteAll();
+//
+//        // Act
+//        Autor result = autorService.create((request));
+//
+//        //Assert
+//        assertNotNull(result);
+//        assertEquals(request.nome(), result.getNome());
+//        assertEquals(request.email(), result.getEmail());
+//    }
+//
+//    @Test
+//    @DisplayName("Deve tentar cadastrar um autor com o mesmo email e deverá retornar uma exceção")
+//    void testDeveTentarCriarUmAutorComOMesmoEmailERetornarExcecao() {
+//        AutorRepository autorRepository = mock(AutorRepository.class);
+//        AutorService autorService = new AutorService(autorRepository);
+//
+//        CadastroAutorRequest autorForm = new CadastroAutorRequest("Nome do Autor", "autor@example.com", "Descrição");
+//
+//        /// Simular que a validação customizada dispara
+//        doThrow(new DataIntegrityViolationException("Existe autor cadastrado para o e-mail informado!"))
+//                .when(autorRepository).save(any());
+//
+//        // Act and Assert
+//        DataIntegrityViolationException exception = assertThrows(DataIntegrityViolationException.class,
+//                () -> autorService.create(autorForm));
+//
+//        // Verificar se a exceção contém a mensagem esperada
+//        assertNotNull(exception.getMessage());
+//        assertTrue(exception.getMessage().contains("Existe autor cadastrado para o e-mail informado!"));
+//
+//        // Verificar se o método save não foi chamado
+//        verify(autorRepository).save(any());
+//    }
+
+    @ParameterizedTest
+    @MethodSource("provideAutorRequests")
+    @DisplayName("Dado os parâmetros para criação, deverá ser possível cadastrar um autor e validar o e-mail.")
+    public void testCreateAutor(CadastroAutorRequest autorForm, boolean emailExists, String expectedExceptionMessage) {
+        // Arrange
         AutorRepository autorRepository = mock(AutorRepository.class);
         AutorService autorService = new AutorService(autorRepository);
 
-        CadastroAutorRequest autorForm = new CadastroAutorRequest("Nome do Autor", "autor@example.com", "Descrição");
+        if (emailExists) {
+            // Simular que a validação customizada dispara
+            doThrow(new DataIntegrityViolationException(expectedExceptionMessage)).when(autorRepository).save(any());
+        } else {
+            // Simular a criação de um novo autor
+            Autor autor = new Autor();
+            autor.setNome(autorForm.nome());
+            autor.setEmail(autorForm.email());
+            autor.setDescricao(autorForm.descricao());
+            when(autorRepository.save(any(Autor.class))).thenReturn(autor);
+        }
+        if (expectedExceptionMessage != null) {
+            // Act and Assert
+            DataIntegrityViolationException exception = assertThrows(DataIntegrityViolationException.class,
+                    () -> autorService.create(autorForm));
 
-        /// Simular que a validação customizada dispara
-        doThrow(new DataIntegrityViolationException("Existe autor cadastrado para o e-mail informado!"))
-                .when(autorRepository).save(any());
+            // Verificar se a exceção contém a mensagem esperada da validação customizada
+            assertNotNull(exception.getMessage());
+            assertTrue(exception.getMessage().contains(expectedExceptionMessage));
+        } else {
+            // Act
+            Autor result = autorService.create(autorForm);
 
-        // Act and Assert
-        DataIntegrityViolationException exception = assertThrows(DataIntegrityViolationException.class,
-                () -> autorService.create(autorForm));
+            // Assert
+            assertNotNull(result);
+            assertEquals(autorForm.nome(), result.getNome());
+            assertEquals(autorForm.email(), result.getEmail());
+        }
 
-        // Verificar se a exceção contém a mensagem esperada
-        assertNotNull(exception.getMessage());
-        assertTrue(exception.getMessage().contains("Existe autor cadastrado para o e-mail informado!"));
-
-        // Verificar se o método save não foi chamado
-        verify(autorRepository).save(any());
+        // Verificar se o método save foi chamado (ou não, dependendo do caso)
+        verify(autorRepository).save(any(Autor.class));
     }
 
 }
