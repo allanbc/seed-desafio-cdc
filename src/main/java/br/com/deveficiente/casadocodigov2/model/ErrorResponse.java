6ba681a08b3540ce605eb6e1aa7f3e338e930ae5
Allan Campos <allan.barrosc@gmail.com>
2025-06-06 00:30:17 -0300

    refactor: remoção do lombok

--- src/main/java/br/com/deveficiente/casadocodigov2/model/ErrorResponse.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/model/ErrorResponse.java
@@ -1,6 +1,7 @@
 package br.com.deveficiente.casadocodigov2.model;
+
 import lombok.Builder;
-import lombok.Data;
+
 import java.util.Map;
 @Builder
 public record ErrorResponse( Integer httpStatusCode, String errorCode, String message, Map<String, String> fields) { 
