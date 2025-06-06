5257ec35f7533c89a4f09054459d017f48ba1d8a
Allan Campos <allan.barrosc@gmail.com>
2025-06-06 00:31:27 -0300

    refactor: remoção do Optional

--- src/main/java/br/com/deveficiente/casadocodigov2/repository/AutorRepository.java
+++ src/main/java/br/com/deveficiente/casadocodigov2/repository/AutorRepository.java
@@ -4,8 +4,6 @@ import br.com.deveficiente.casadocodigov2.entity.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
 }
